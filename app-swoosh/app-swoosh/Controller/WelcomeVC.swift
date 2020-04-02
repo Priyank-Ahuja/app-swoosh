b10b0e7ac4680dd961da56ec97918805d3a671c9
pravirahuja <ahujapravir@gmail.com>
2020-04-02 17:06:14 +0530

    debugging setValue for UndefinedKey

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -16,9 +16,6 @@ class WelcomeVC: UIViewController {
 
     }
 
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var BgImg: UIImageView!
-    
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
         
     }
